03699196d7d9c131c615c6bb2c2b0276e0284c47
master-harvey <ianpadraicharvey@gmail.com>
2024-10-07 14:20:25 -0600

    new URL + github link

--- path_collector/src/main.tsx
+++ path_collector/src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+import { FaGithub } from "react-icons/fa";
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1 className="heading">Cursor Path Data Collection Mechanism</h1>
+    <h1 className="heading">
+      Cursor Path Data Collection Mechanism &nbsp;
+      <a href="https://github.com/master-harvey/Human-Tweening" target="_blank" className="github">
+        <FaGithub />
+      </a>
+    </h1>
     <App />
   </React.StrictMode>,
 )
